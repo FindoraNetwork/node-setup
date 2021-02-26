0b4483cb8a958ab3bfca02a39957e513d51d9deb
Jimmy <jimmy@findora.org>
2021-02-26 15:02:38 -0800

    remove the toml part

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
 sudo apt update
-sudo apt install python3
-sudo apt install python3-pip
-sudo pip3 install toml
 sudo apt install docker.io
 sudo apt install docker-compose
 
@@ -15,9 +12,9 @@ sudo chown 100 /mnt/data/tendermint
 sudo chmod 0770 /mnt/data/tendermint
 sudo docker run -it --rm -v "/mnt/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
 
-echo Please enter the name of the node:
-read node_name
-toml set --toml-path config.toml moniker node_name
+
 cp config.toml /mnt/data/tendermint/mainnet/node0/config/config.toml
 cp genesis.json /mnt/data/tendermint/mainnet/node0/config/genesis.json
 cp priv_validator_state.json /mnt/data/tendermint/mainnet/node0/data/priv_validator_state.json
+
+docker-compose up -d
