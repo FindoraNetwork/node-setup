7c26908ffeedcc876805555cd8c829ffb1b4d149
Jimmy <jimmy@findora.org>
2021-04-03 21:28:03 -0700

    fix the path error

--- init.sh
+++ init.sh
@@ -13,24 +13,24 @@ function alpha()
 
 
     # init the tendermint
-    sudo mkdir -p /mnt/data/tendermint
-    sudo mkdir -p /mnt/data/ledger/
-    sudo chown 100 /mnt/data/tendermint
-    sudo chmod 0770 /mnt/data/tendermint
-    sudo docker run -it --rm -v "/mnt/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
+    sudo mkdir -p /mnt/findora-alpha/data/tendermint
+    sudo mkdir -p /mnt/findora-alpha/data/ledger/
+    sudo chown 100 /mnt/findora-alpha/data/tendermint
+    sudo chmod 0770 /mnt/findora-alpha/data/tendermint
+    sudo docker run -it --rm -v "/mnt/findora-alpha/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
 
-    sudo cp config/alpha/config.toml /mnt/data/tendermint/mainnet/node0/config/config.toml
-    sed -i'.original' -e "s/CHANGE_NODE_NAME/${moniker}/g" /mnt/data/tendermint/mainnet/node0/config/config.toml
+    sudo cp config/alpha/config.toml /mnt/findora-alpha/data/tendermint/mainnet/node0/config/config.toml
+    sed -i'.original' -e "s/CHANGE_NODE_NAME/${node_name}/g" /mnt/findora-alpha/data/tendermint/mainnet/node0/config/config.toml
 
-    sudo cp config/alpha/genesis.json /mnt/data/tendermint/mainnet/node0/config/genesis.json
+    sudo cp config/alpha/genesis.json /mnt/findora-alpha/data/tendermint/mainnet/node0/config/genesis.json
 
-    sudo cp config/alpha/priv_validator_state.json /mnt/data/tendermint/mainnet/node0/data/priv_validator_state.json
+    sudo cp config/alpha/priv_validator_state.json /mnt/findora-alpha/data/tendermint/mainnet/node0/data/priv_validator_state.json
     cd config/alpha
     sudo docker-compose down
     sudo docker-compose up -d
     echo "Full node for Alpha setup done. "
-    echo "Ledger data: /mnt/data/ledger/"
-    echo "Tendermint data: /mnt/data/tendermint"
+    echo "Ledger data: /mnt/findora-alpha/data/ledger/"
+    echo "Tendermint data: /mnt/findora-alpha/data/tendermint"
 }
 
 function mainnet()
@@ -44,19 +44,19 @@ function mainnet()
     echo "Docker compose install done."
 
     # init the tendermint
-    sudo mkdir -p /mnt/data/tendermint
-    sudo mkdir -p /mnt/data/ledger/
-    sudo chown 100 /mnt/data/tendermint
-    sudo chmod 0770 /mnt/data/tendermint
-    sudo docker run -it --rm -v "/mnt/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
+    sudo mkdir -p /mnt/findora-mainnet/data/tendermint
+    sudo mkdir -p /mnt/findora-mainnet/data/ledger/
+    sudo chown 100 /mnt/findora-mainnet/data/tendermint
+    sudo chmod 0770 /mnt/findora-mainnet/data/tendermint
+    sudo docker run -it --rm -v "/mnt/findora-mainnet/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
     
     
-    sudo cp config/mainnet/config.toml /mnt/data/tendermint/mainnet/node0/config/config.toml
-    sed -i'.original' -e "s/CHANGE_NODE_NAME/${moniker}/g" /mnt/data/tendermint/mainnet/node0/config/config.toml
+    sudo cp config/mainnet/config.toml /mnt/findora-mainnet/data/tendermint/mainnet/node0/config/config.toml
+    sed -i'.original' -e "s/CHANGE_NODE_NAME/${moniker}/g" /mnt/findora-mainnet/data/tendermint/mainnet/node0/config/config.toml
 
-    sudo cp config/mainnet/genesis.json /mnt/data/tendermint/mainnet/node0/config/genesis.json
+    sudo cp config/mainnet/genesis.json /mnt/findora-mainnet/data/tendermint/mainnet/node0/config/genesis.json
 
-    sudo cp config/mainnet/priv_validator_state.json /mnt/data/tendermint/mainnet/node0/data/priv_validator_state.json
+    sudo cp config/mainnet/priv_validator_state.json /mnt/findora-mainnet/data/tendermint/mainnet/node0/data/priv_validator_state.json
     cd config/mainnet
     sudo docker-compose down
     sudo docker-compose up -d
