d85418911f6b72580c773793da2bff8e165dfb05
Jimmy <jimmy@findora.org>
2021-04-03 20:56:32 -0700

    Update the mainnet and script

--- init.sh
+++ init.sh
@@ -1,25 +1,97 @@
 #!/bin/sh
 
+echo "Installing docker and docker-compose"
+# sudo apt update
+# sudo apt -y install docker.io
+# sudo apt -y install docker-compose
 
-sudo apt update
-sudo apt -y install docker.io
-sudo apt -y install docker-compose
 
 
-sudo rm -r /mnt/data/tendermint
-sudo rm -r /mnt/data/ledger
-# init the tendermint
-sudo mkdir -p /mnt/data/tendermint
-sudo mkdir -p /mnt/data/ledger/
-sudo chown 100 /mnt/data/tendermint
-sudo chmod 0770 /mnt/data/tendermint
-sudo docker run -it --rm -v "/mnt/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
+PS3='Welcome to Findora full node setup tool. Please selet the network: '
+options=("Alpha" "Mainnet" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            alpha
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
 
-sudo cp config/config.toml /mnt/data/tendermint/mainnet/node0/config/config.toml
 
-sudo cp config/genesis.json /mnt/data/tendermint/mainnet/node0/config/genesis.json
+function alpha()
+{
+    read -p "Please name the full node:" node_name
+    read -p "The path you want to setup the full node(Use absolute path, default /mnt, do not add '/' at the end of the path):" node_path
+    if [ -z "$node_path" ];then
+        node_path="/mnt"
+    fi
+    echo "Installing the docker and docker-compose."
+    sudo apt update
+    sudo apt -y install docker.io
+    sudo apt -y install docker-compose
+    echo "Docker compose install done."
 
-sudo cp config/priv_validator_state.json /mnt/data/tendermint/mainnet/node0/data/priv_validator_state.json
 
-sudo docker-compose down
-sudo docker-compose up -d
+    # init the tendermint
+    sudo mkdir -p $node_path/data/tendermint
+    sudo mkdir -p $node_path/data/ledger/
+    sudo chown 100 $node_path/data/tendermint
+    sudo chmod 0770 $node_path/data/tendermint
+    sudo docker run -it --rm -v "$node_path/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
+
+    sudo cp config/alpha/config.toml $node_path/data/tendermint/mainnet/node0/config/config.toml
+    sed -i'.original' -e "s/CHANGE_NODE_NAME/${moniker}/g" $node_path/data/tendermint/mainnet/node0/config/config.toml
+
+    sudo cp config/alpha/genesis.json $node_path/data/tendermint/mainnet/node0/config/genesis.json
+
+    sudo cp config/alpha/priv_validator_state.json $node_path/data/tendermint/mainnet/node0/data/priv_validator_state.json
+    sudo cd config
+    sudo docker-compose down
+    sudo docker-compose up -d
+    echo "Full node for Alpha setup done. "
+    echo "Ledger data: /mnt/data/ledger/"
+    echo "Tendermint data: /mnt/data/tendermint"
+}
+
+function mainnet()
+{
+    read -p "Please name the full node:" node_name
+    read -p "The path you want to setup the full node(Use absolute path, default /mnt, do not add '/' at the end of the path):" node_path
+    if [ -z "$node_path" ];then
+        node_path="/mnt"
+    fi
+    echo "Installing the docker and docker-compose."
+    sudo apt update
+    sudo apt -y install docker.io
+    sudo apt -y install docker-compose
+    echo "Docker compose install done."
+
+    # init the tendermint
+    sudo mkdir -p $node_path/data/tendermint
+    sudo mkdir -p $node_path/data/ledger/
+    sudo chown 100 $node_path/data/tendermint
+    sudo chmod 0770 $node_path/data/tendermint
+    sudo docker run -it --rm -v "$node_path/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
+    
+    
+    sudo cp config/mainnet/config.toml $node_path/data/tendermint/mainnet/node0/config/config.toml
+    sed -i'.original' -e "s/CHANGE_NODE_NAME/${moniker}/g" $node_path/data/tendermint/mainnet/node0/config/config.toml
+
+    sudo cp config/mainnet/genesis.json $node_path/data/tendermint/mainnet/node0/config/genesis.json
+
+    sudo cp config/mainnet/priv_validator_state.json $node_path/data/tendermint/mainnet/node0/data/priv_validator_state.json
+    sudo cd config
+    sudo docker-compose down
+    sudo docker-compose up -d
+    echo "Full node for Alpha setup done. "
+    echo "Ledger data: /mnt/data/ledger/"
+    echo "Tendermint data: /mnt/data/tendermint"
+}
