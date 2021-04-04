99d63b1d9f173928561bae1bf3b3ff4c44d66b68
Jimmy <jimmy@findora.org>
2021-04-03 21:19:49 -0700

    fix the path issue

--- init.sh
+++ init.sh
@@ -4,10 +4,7 @@
 function alpha()
 {
     read -p "Please name the full node:" node_name
-    read -p "The path you want to setup the full node(Use absolute path, default /mnt, do not add '/' at the end of the path):" node_path
-    if [ -z "$node_path" ];then
-        node_path="/mnt"
-    fi
+
     echo "Installing the docker and docker-compose."
     sudo apt update
     sudo apt -y install docker.io
@@ -16,19 +13,19 @@ function alpha()
 
 
     # init the tendermint
-    sudo mkdir -p $node_path/data/tendermint
-    sudo mkdir -p $node_path/data/ledger/
-    sudo chown 100 $node_path/data/tendermint
-    sudo chmod 0770 $node_path/data/tendermint
-    sudo docker run -it --rm -v "$node_path/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
+    sudo mkdir -p /mnt/data/tendermint
+    sudo mkdir -p /mnt/data/ledger/
+    sudo chown 100 /mnt/data/tendermint
+    sudo chmod 0770 /mnt/data/tendermint
+    sudo docker run -it --rm -v "/mnt/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
 
-    sudo cp config/alpha/config.toml $node_path/data/tendermint/mainnet/node0/config/config.toml
-    sed -i'.original' -e "s/CHANGE_NODE_NAME/${moniker}/g" $node_path/data/tendermint/mainnet/node0/config/config.toml
+    sudo cp config/alpha/config.toml /mnt/data/tendermint/mainnet/node0/config/config.toml
+    sed -i'.original' -e "s/CHANGE_NODE_NAME/${moniker}/g" /mnt/data/tendermint/mainnet/node0/config/config.toml
 
-    sudo cp config/alpha/genesis.json $node_path/data/tendermint/mainnet/node0/config/genesis.json
+    sudo cp config/alpha/genesis.json /mnt/data/tendermint/mainnet/node0/config/genesis.json
 
-    sudo cp config/alpha/priv_validator_state.json $node_path/data/tendermint/mainnet/node0/data/priv_validator_state.json
-    sudo cd config
+    sudo cp config/alpha/priv_validator_state.json /mnt/data/tendermint/mainnet/node0/data/priv_validator_state.json
+    cd config/alpha
     sudo docker-compose down
     sudo docker-compose up -d
     echo "Full node for Alpha setup done. "
@@ -39,10 +36,7 @@ function alpha()
 function mainnet()
 {
     read -p "Please name the full node:" node_name
-    read -p "The path you want to setup the full node(Use absolute path, default /mnt, do not add '/' at the end of the path):" node_path
-    if [ -z "$node_path" ];then
-        node_path="/mnt"
-    fi
+
     echo "Installing the docker and docker-compose."
     sudo apt update
     sudo apt -y install docker.io
@@ -50,20 +44,20 @@ function mainnet()
     echo "Docker compose install done."
 
     # init the tendermint
-    sudo mkdir -p $node_path/data/tendermint
-    sudo mkdir -p $node_path/data/ledger/
-    sudo chown 100 $node_path/data/tendermint
-    sudo chmod 0770 $node_path/data/tendermint
-    sudo docker run -it --rm -v "$node_path/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
+    sudo mkdir -p /mnt/data/tendermint
+    sudo mkdir -p /mnt/data/ledger/
+    sudo chown 100 /mnt/data/tendermint
+    sudo chmod 0770 /mnt/data/tendermint
+    sudo docker run -it --rm -v "/mnt/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
     
     
-    sudo cp config/mainnet/config.toml $node_path/data/tendermint/mainnet/node0/config/config.toml
-    sed -i'.original' -e "s/CHANGE_NODE_NAME/${moniker}/g" $node_path/data/tendermint/mainnet/node0/config/config.toml
+    sudo cp config/mainnet/config.toml /mnt/data/tendermint/mainnet/node0/config/config.toml
+    sed -i'.original' -e "s/CHANGE_NODE_NAME/${moniker}/g" /mnt/data/tendermint/mainnet/node0/config/config.toml
 
-    sudo cp config/mainnet/genesis.json $node_path/data/tendermint/mainnet/node0/config/genesis.json
+    sudo cp config/mainnet/genesis.json /mnt/data/tendermint/mainnet/node0/config/genesis.json
 
-    sudo cp config/mainnet/priv_validator_state.json $node_path/data/tendermint/mainnet/node0/data/priv_validator_state.json
-    sudo cd config
+    sudo cp config/mainnet/priv_validator_state.json /mnt/data/tendermint/mainnet/node0/data/priv_validator_state.json
+    cd config/mainnet
     sudo docker-compose down
     sudo docker-compose up -d
     echo "Full node for Alpha setup done. "
