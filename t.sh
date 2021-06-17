a1551b56cb7b41bd67ea6588e12e4ce170433d2a
Jimmy <jimmy@findora.org>
2021-06-17 00:31:59 -0700

    full node on qa01

--- t.sh
+++ t.sh
@@ -8,21 +8,21 @@
 
 
     # init the tendermint
-    sudo mkdir -p /mnt/findora-alpha/data/tendermint
-    sudo mkdir -p /mnt/findora-alpha/data/ledger/
-    sudo chown 100 /mnt/findora-alpha/data/tendermint
-    sudo chmod 0770 /mnt/findora-alpha/data/tendermint
-    sudo docker run -it --rm -v "/mnt/findora-alpha/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
+    sudo mkdir -p /mnt/findora-qa01/data/tendermint
+    sudo mkdir -p /mnt/findora-qa01/data/ledger/
+    sudo chown 100 /mnt/findora-qa01/data/tendermint
+    sudo chmod 0770 /mnt/findora-qa01/data/tendermint
+    sudo docker run -it --rm -v "/mnt/findora-qa01/data/tendermint:/tendermint" tendermint/tendermint:v0.33.1 testnet --o mainnet --n 1 --v 0
 
-    sudo cp config/alpha/config.toml /mnt/findora-alpha/data/tendermint/mainnet/node0/config/config.toml
-    sed -i'.original' -e "s/CHANGE_NODE_NAME/${node_name}/g" /mnt/findora-alpha/data/tendermint/mainnet/node0/config/config.toml
+    sudo cp config/qa01/config.toml /mnt/findora-qa01/data/tendermint/mainnet/node0/config/config.toml
+    # sed -i'.original' -e "s/CHANGE_NODE_NAME/${node_name}/g" /mnt/findora-qa01/data/tendermint/mainnet/node0/config/config.toml
 
-    sudo cp config/alpha/genesis.json /mnt/findora-alpha/data/tendermint/mainnet/node0/config/genesis.json
+    sudo cp config/qa01/genesis.json /mnt/findora-qa01/data/tendermint/mainnet/node0/config/genesis.json
 
-    sudo cp config/alpha/priv_validator_state.json /mnt/findora-alpha/data/tendermint/mainnet/node0/data/priv_validator_state.json
-    cd config/alpha
+    sudo cp config/qa01/priv_validator_state.json /mnt/findora-qa01/data/tendermint/mainnet/node0/data/priv_validator_state.json
+    cd config/qa01
     sudo docker-compose down
     sudo docker-compose up -d
-    echo "Full node for Alpha setup done. "
-    echo "Ledger data: /mnt/findora-alpha/data/ledger/"
-    echo "Tendermint data: /mnt/findora-alpha/data/tendermint"
+    echo "Full node for qa01 setup done. "
+    echo "Ledger data: /mnt/findora-qa01/data/ledger/"
+    echo "Tendermint data: /mnt/findora-qa01/data/tendermint"
